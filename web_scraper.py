7da9b611e3d7c58d13fcef640cb19c0446e95040
Will Xiong <will_xiong@hotmail.com>
2020-08-17 19:44:51 -0700

    Added support for row entry without symbols or company information

--- web_scraper.py
+++ web_scraper.py
@@ -5,8 +5,7 @@ import requests
 import os
 
 insider_trades = []
-
-
+trading_activity = {'B': 'Buy', 'S': 'Sell', 'O': 'Options Excersise'}
 def parse_row_info(trades):
     """
     :param trades:
@@ -15,13 +14,22 @@ def parse_row_info(trades):
     Value, Trade Date & Time
     :return:
     """
-    trading_activity = {'B': 'Buy', 'S': 'Sell', 'O': 'Options Excersise'}
-    symbol = trades[0]
+    # Check to see if it contains symbol and company info, otherwise use previous
+    if len(trades[0]) > 1:
+        symbol = trades[0]
+        company = trades[1].split('(' + symbol + ')')[0]
+        company = company.strip()
+    else:
+        last_trade = insider_trades[-1]
+        symbol = last_trade[0]
+        company = last_trade[1]
 
-    company = trades[1].split('(' + symbol + ')')[0]
-    company = company.strip()
+    if '(' in trades[2]:
+        insider, insider_position = trades[2].split("(")
+    else:
+        insider = trades[2]
+        insider_position = ''
 
-    insider, insider_position = trades[2].split("(")
     insider = insider.strip()
     insider_position = insider_position[:-1]
 
@@ -31,11 +39,11 @@ def parse_row_info(trades):
     trade_shares = float(trade_shares.replace(",",""))
     trade_price = float(trade_price.replace(",",""))
 
-    trade_date = datetime.strptime(trades[6],'%y-%m-%d%H:%M:%S')
+    trade_date = datetime.strptime(trades[6],'%Y-%m-%d%H:%M:%S')
 
-    insider_trades.append[
-        [symbol, company, insider, insider_position, trade_type, trade_shares, trade_price, trade_date]]
-    pass
+    insider_trades.append(
+        [symbol, company, insider, insider_position, trade_type, trade_shares, trade_price, trade_date])
+    return
 
 
 def main():
@@ -50,7 +58,6 @@ def main():
         trade = row.find_all('td')
         row_info = [x.text.strip() for x in trade]
         parse_row_info(row_info)
-
 
 if __name__ == "__main__":
     main()
